4f66f223d2891b555e2a43f69e020b720bd0aa89
rahuldulgaj <[Your>
2021-01-19 14:37:26 +0530

    no message

--- ev-stations/database/seeds/DatabaseSeeder.php
+++ ev-stations/database/seeds/DatabaseSeeder.php
@@ -24,6 +24,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(AmenitiesSeeder::class);
         $this->call(ModelTypeSeeder::class);
-        $this->call(NetworkTypesSeeder::class);
+    //    $this->call(NetworkTypesSeeder::class);
     }
 }
