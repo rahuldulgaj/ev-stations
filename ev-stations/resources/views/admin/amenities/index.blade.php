2065973f31b407b16b61a932bc514ad0afdf8365
rahuldulgaj <[Your>
2021-01-19 14:59:25 +0530

    amenity

--- ev-stations/resources/views/admin/amenities/index.blade.php
+++ ev-stations/resources/views/admin/amenities/index.blade.php
@@ -109,7 +109,7 @@
                                             <a href="{{route('admin.amenities.edit',$amenity->id)}}" class="btn btn-sm btn-info">Edit</a>
                                             <a href="{{route('admin.amenities.show',$amenity->id)}}" class="btn btn-success btn-sm waves-effect">View</a>                                        </a>
                                           
-                                            <form id="delete-form-{{ $amenity->id }}" action="{{route('admin.amenities.destroy',$amenitie->id)}}" method="put">
+                                            <form id="delete-form-{{ $amenity->id }}" action="{{route('admin.amenities.destroy',$amenity->id)}}" method="put">
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="button" onclick="deletePost({{ $amenity->id }})" class="btn btn-sm btn-danger">Delete</button>
